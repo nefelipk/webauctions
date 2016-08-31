1cc14df5d682032f7714b81c97885422484278a9
ripone07 <dimitriskoumantos@gmail.com>
2016-08-31 18:03:03 +0300

    finally accomplished to get footer fixed to bottom of any page BUT bot sticky(prefered). some messages' view empellishments

--- WebContent/app/components/messages/messages.js
+++ WebContent/app/components/messages/messages.js
@@ -3,4 +3,22 @@ angular.module('auction_land').controller('MessagesController',['$scope',functio
 	$scope.pages = [1,2];		
 	$scope.current_page = 1;
 	$scope.selected = false;
+	
+	$scope.set_active = function(tab) {
+		var inbox_tab = angular.element(document.querySelector('#inbox_tab'));
+		var sent_tab = angular.element(document.querySelector('#sent_tab'));
+		var compose_tab = angular.element(document.querySelector('#compose_tab'));
+		inbox_tab.removeClass('active');
+		sent_tab.removeClass('active');
+		compose_tab.removeClass('active');
+		
+		if(tab == 1)
+			compose_tab.addClass('active');
+		else if(tab == 2)
+			inbox_tab.addClass('active');
+		else if(tab == 3)
+			sent_tab.addClass('active');
+
+	};
+
 }]);
