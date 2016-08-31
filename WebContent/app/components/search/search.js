606d93bdf6f72839f5708c452191559fb3973e72
ripone07 <dimitriskoumantos@gmail.com>
2016-08-31 21:52:50 +0300

    even some more eyecandy , loading animation

--- WebContent/app/components/search/search.js
+++ WebContent/app/components/search/search.js
@@ -1,7 +1,10 @@
 angular.module('auction_land').controller('SearchController',['$scope','$route','localStorageService','$location','Item','SearchService',
 	                                   function($scope,$route,localStorageService,$location,Item,SearchService) {
+		$scope.searching = false;
 		$scope.search = function(term) {
+			$scope.searching = true;
 			Item.query({term : term}).$promise.then(function (data) {
+				$scope.searching = false;
 				localStorageService.remove('auctions');
 				$scope.items = data.slice();
 				SearchService.add_items($scope.items);
