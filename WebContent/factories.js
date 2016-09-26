ddd09a238c7110f60c568776f8098bc73cf518dc
ripone07 <dimitriskoumantos@gmail.com>
2016-09-26 17:33:50 +0300

    urls for ssl connections. ballbreaking problems with chrome and ssl + chrome extension got to be installed. all ignored

--- WebContent/factories.js
+++ WebContent/factories.js
@@ -2,11 +2,20 @@
 angular.module('auction_land').factory('User', [ '$resource', function($resource) {
 	return $resource('http://localhost:8080/WebAuctions/services/users/:username');
 } ]);
-
+/*
+angular.module('auction_land').factory('User', [ '$resource', function($resource) {
+	return $resource('https://localhost:8443/WebAuctions/services/users/:username');
+} ]);
+*/
 angular.module('auction_land').factory('UserLogin', [ '$resource', function($resource) {
 	return $resource('http://localhost:8080/WebAuctions/services/users/login/');
 } ]);
 
+/*
+angular.module('auction_land').factory('UserLogin', [ '$resource', function($resource) {
+	return $resource('https://localhost:8443/WebAuctions/services/users/login/');
+} ]);
+*/
 angular.module('auction_land').factory('AllUsers', [ '$resource', function($resource) {
 	return $resource('http://localhost:8080/WebAuctions/services/users/allusers/');
 } ]);
