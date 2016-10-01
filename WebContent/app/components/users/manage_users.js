c7fab6fff6e7ef22d7fb4943d247d898c8e01047
Nefeli Prokopaki-Kostopoulou <nefelipk@yahoo.com>
2016-10-01 21:54:23 +0300

    Done with managing users

--- WebContent/app/components/users/manage_users.js
+++ WebContent/app/components/users/manage_users.js
@@ -79,45 +79,4 @@ angular.module('auction_land').controller('UserManagerController',
 		});
 	};
 	
-	
-	/*********************************************************************/
-	/**********************************************************************
-								PAGINATION 
-	**********************************************************************/
-	/*********************************************************************/
-
-//	
-//	var users_per_page = 10;
-//	$scope.current_page = 1;
-//	
-//	$scope.fix_pages = function() {
-//		$scope.pages = [];
-//		for(i = 0; i < $scope.presented_messages.length/users_per_page; i++)
-//			$scope.pages.push(i+1);
-//		$scope.last_page = $scope.pages[$scope.pages.length-1];
-//		$scope.current_page = 1;
-//	};
-//	
-//	$scope.get_items = function() {
-//		var from = ($scope.current_page - 1) * users_per_page;
-//		var to = $scope.current_page * users_per_page;
-//		if ($scope.current_page * users_per_page >= $scope.presented_messages.length)
-//			to = $scope.presented_messages.length;
-//		return $scope.presented_messages.slice(from, to);
-//	};
-//	
-//	$scope.get_page = function(page_num) {
-//		$scope.current_page = page_num;
-//		$scope.current_items = $scope.get_items();
-//		$window.scrollTo(0,0);	
-//	}; 
-//	
-//
-//	$scope.current_items = $scope.get_items();
-	
-	/*********************************************************************/
-	/*********************************************************************/
-	/*********************************************************************/
-	/*********************************************************************/
-	
 }]);
